2fe8fc04e92f5313d2f0dd8a94db8e4803c394c7
Tiago Cardoso <tbcardoso@outlook.com>
2017-03-08 18:06:28 +0000

    Add toolbar action to open file.

--- src/routes/TextViewer/components/TextViewer.js
+++ src/routes/TextViewer/components/TextViewer.js
@@ -5,21 +5,52 @@ import {
 } from 'react-native';
 import TxtViewContainer from '../containers/TxtViewContainer'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import {FilePickerManager} from 'NativeModules';
 
 
 export default class TextViewer extends React.Component {
+
+	_onActionSelected = (position) => {
+		console.log('Position = ', position);
+		switch (position) {
+			case 0:
+				FilePickerManager.showFilePicker(null, (response) => {
+					console.log('Response = ', response);
+
+					if (response.didCancel) {
+						console.log('User cancelled file picker');
+					}
+					else if (response.error) {
+						console.log('FilePickerManager Error: ', response.error);
+					}
+					else {
+						this.setState({
+							file: response
+						});
+					}
+				});
+				break;
+		}
+	}
 
 	render() {
 		return (
 			<View style={{ flex: 1 }}>
 				<Icon.ToolbarAndroid
 					title="SimpleTXT"
-					style={styles.toolbar} />
+					style={styles.toolbar}
+					actions={toolbarActions}
+					onActionSelected={this._onActionSelected} />
 				<TxtViewContainer style={{ flex: 1 }} />
 			</View>
 		);
 	}
 }
+
+const toolbarActions = [
+	{ title: 'Open File', iconName: 'insert-drive-file', iconSize: 30, show: 'always' },
+];
+
 
 const styles = StyleSheet.create({
 	toolbar: {
