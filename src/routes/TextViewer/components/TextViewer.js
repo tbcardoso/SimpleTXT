7cb112ec58c8a751148be0f144ad9cbc15a69730
Tiago Cardoso <tbcardoso@outlook.com>
2017-03-08 15:56:43 +0000

    Add toolbar to TextViewer component.

--- src/routes/TextViewer/components/TextViewer.js
+++ src/routes/TextViewer/components/TextViewer.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {
+	StyleSheet,
+	View,
+} from 'react-native';
 import TxtViewContainer from '../containers/TxtViewContainer'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 
 export default class TextViewer extends React.Component {
 
 	render() {
 		return (
-			<TxtViewContainer/>
+			<View style={{ flex: 1 }}>
+				<Icon.ToolbarAndroid
+					title="SimpleTXT"
+					style={styles.toolbar} />
+				<TxtViewContainer style={{ flex: 1 }} />
+			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	toolbar: {
+		backgroundColor: '#a9a9a9',
+		height: 56,
+	},
+});
