3085f480898f8bb79e23586ce95af6d5b7a6a1e7
Tiago Cardoso <tbcardoso@outlook.com>
2017-03-08 19:05:43 +0000

    Add react-native-fetch-blob dependency

--- android/app/src/main/java/com/simpletxt/MainApplication.java
+++ android/app/src/main/java/com/simpletxt/MainApplication.java
@@ -2,10 +2,10 @@ package com.simpletxt;
 
 import android.app.Application;
 
+import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.filepicker.FilePickerPackage;
-
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNFetchBlobPackage(),
           new VectorIconsPackage(),
           new FilePickerPackage()
       );
