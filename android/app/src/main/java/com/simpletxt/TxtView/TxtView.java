5e539cabebd0d3a3abc63a56cda7a2e9ef5139f1
Tiago Cardoso <tbcardoso@outlook.com>
2017-03-12 20:18:09 +0000

    Make TxtView selectable and scrollable.

--- android/app/src/main/java/com/simpletxt/TxtView/TxtView.java
+++ android/app/src/main/java/com/simpletxt/TxtView/TxtView.java
@@ -54,6 +54,22 @@ public class TxtView extends FrameLayout {
         txtContent.setText(fileContent);
     }
 
+    @Override
+    public void requestLayout() {
+        super.requestLayout();
+        post(measureAndLayout);
+    }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
+
     public void setFileContent(String fileContent) {
         txtContent.setText(fileContent);
     }
